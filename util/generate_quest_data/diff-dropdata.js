df4b41ebfa934cbc89397b7c7bfc9b6420fbfa2f
kanata3249 <kanata3249@gmail.com>
2021-08-10 00:29:17 +0900

    fix crash in diff-dropdata

--- util/generate_quest_data/diff-dropdata.js
+++ util/generate_quest_data/diff-dropdata.js
@@ -18,12 +18,20 @@ console.log('chapter\tquest\titem\told\tnew')
 Object.keys(newDropData).forEach((questId) => {
   const dropRates = newDropData[questId]
   const oldDropRates = oldDropData[questId]
-  if (JSON.stringify(dropRates) != JSON.stringify(oldDropRates)) {
+  if (oldDropRates && Object.keys(oldDropRates).length > 0) {
+    if (JSON.stringify(dropRates) != JSON.stringify(oldDropRates)) {
+      Object.keys(dropRates).forEach((itemId) => {
+        if (itemId >= 300 && itemId < 600) {
+          if (Math.abs(parseFloat(dropRates[itemId]) - parseFloat(oldDropRates[itemId])) >= delta) {
+            console.log(`${questNames[questId]}\t${itemNames[itemId]}\t${oldDropRates[itemId]}\t${dropRates[itemId]}`)
+          }
+        }
+      })
+    }
+  } else {
     Object.keys(dropRates).forEach((itemId) => {
       if (itemId >= 300 && itemId < 600) {
-        if (Math.abs(parseFloat(dropRates[itemId]) - parseFloat(oldDropRates[itemId])) >= delta) {
-          console.log(`${questNames[questId]}\t${itemNames[itemId]}\t${oldDropRates[itemId]}\t${dropRates[itemId]}`)
-        }
+        console.log(`${questNames[questId]}\t${itemNames[itemId]}\t0.0\t${dropRates[itemId]}`)
       }
     })
   }
