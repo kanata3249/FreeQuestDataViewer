e6c951f208ef2b969fcad3543b8eec8ae1a53720
kanata3249 <kanata3249@gmail.com>
2024-03-16 21:00:52 +0900

    change csv file origin

--- util/generate_quest_data/downloadQuestData.sh
+++ util/generate_quest_data/downloadQuestData.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SPREADSHEET_ID="1hcTm8x1ipr8y4LWWjkrLf6oS0Jkz16YQ"
+SPREADSHEET_ID="1e_Yur06j7xkcm2vtC7a8ezVR7v3kVyLXg1Ea9xmdOYU"
 
 curl -o input/1_chapter.csv "https://docs.google.com/spreadsheets/d/${SPREADSHEET_ID}/gviz/tq?gid=1703097415&tqx=out:csv"
 curl -o input/2_area.csv "https://docs.google.com/spreadsheets/d/${SPREADSHEET_ID}/gviz/tq?gid=325084492&tqx=out:csv"
